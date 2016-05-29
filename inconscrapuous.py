c3352945691401dc66a024700bf8a734090b0e92
wcpines <colby@aptible.com>
2016-05-29 17:01:18 -0400

    small progress in grouping by date

--- inconscrapuous.py
+++ inconscrapuous.py
@@ -20,24 +20,57 @@ TODO:
 #     url = "http://cheesetrees.net/page/{}".format(number)
 
 
+
 url = "http://cheesetrees.net"
 
 r = requests.get(url)
 html = r.content
 soup = bs4.BeautifulSoup(html, "html.parser")
 
+# - [X]Using the soup object, get the articles
+# - Loop through each article. 
+  # - [X]Get date
+  # - [X]Get id
+  # - [X]Get article_title
+  # - [X]Get link
 
-articles = soup.findAll('h1', {'class': "article_title"})
+# - [ ]Create list of dict (split lists by year here?)
+# - [ ]Transform datefrom 2016/4/01 to [Month name], [Year]
+# [ {id : date, article_title, link} ]
+
+# For x in dict_list 
+
+articles = soup.findAll('article', {'class':'post user_show'})
+
 for article in articles:
-    text = article.getText().strip('\n')
-    link = article('a')[0].get('href').encode('utf-8')
-    protocol = 'http:'
-    full_url = protocol + link
+  id = article.get('id')
+  date = article('time')[0].get('datetime').encode('utf-8')
+  link = article('a')[0].get('href').encode('utf-8')
+  article_title = article('a')[0].getText().encode('utf-8')
+  full_url = protocol + link
 
-    if len(text) > 0:
-        article_title = text.encode('utf-8')
-        mdown_link = '1. [%s](%s)' % (article_title,full_url)
-        title_list.append(mdown_link.strip("'"))
+for article_title in title_list:
+  print article_title
 
-for title in title_list:
-  print title
+protocol = 'http:'
+mdown_link = '1. [%s](%s)' % (article_title,full_url)
+title_list.append(mdown_link.strip("'"))
+
+
+"""
+*--final formatting--*
+#Archive 
+
+##Posts in [year]
+
+###Month 1
+
+-Article link 
+-Article link 
+
+###Month 2
+
+-Article link 
+-Article link 
+"""
+
