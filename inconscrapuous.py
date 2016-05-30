53b6473ab10553ed87438b1811bd968d240bd92b
wcpines <colby@aptible.com>
2016-05-30 00:36:45 -0400

    breaking things

--- inconscrapuous.py
+++ inconscrapuous.py
@@ -3,16 +3,12 @@ import requests
 import bs4
 import time
 
-# create empty list to hold all dictionaries.
-title_list = []
 
 """
 TODO: 
 1. Check if blog spans multiple pages.(Check for <span class="last"> 
  for full range.  If it does, loop through and pull all of them.
 2. Generate markdown file 
-3. Sort by post date: 
-  <time datetime="2016-04-30" class="article_time"> Apr 30, 2016</time>
 """
 
 # for number in range(1,[last_page]):
@@ -20,42 +16,78 @@ TODO:
 #     url = "http://cheesetrees.net/page/{}".format(number)
 
 
+# Create empty lists by year to hold article components. NOTE: This only works for 2015 and 2016.  
+# Need a better way to do this later.
+tf_posts = []
+ts_posts = []
+
 
 url = "http://cheesetrees.net"
-
 r = requests.get(url)
 html = r.content
 soup = bs4.BeautifulSoup(html, "html.parser")
 
-# - [X]Using the soup object, get the articles
-# - Loop through each article. 
-  # - [X]Get date
-  # - [X]Get id
-  # - [X]Get article_title
-  # - [X]Get link
-
-# - [ ]Create list of dict (split lists by year here?)
-# - [ ]Transform datefrom 2016/4/01 to [Month name], [Year]
-# [ {id : date, article_title, link} ]
-
-# For x in dict_list 
-
 articles = soup.findAll('article', {'class':'post user_show'})
 
 for article in articles:
-  id = article.get('id')
-  date = article('time')[0].get('datetime').encode('utf-8')
-  link = article('a')[0].get('href').encode('utf-8')
-  article_title = article('a')[0].getText().encode('utf-8')
-  full_url = protocol + link
+  try:
 
-for article_title in title_list:
-  print article_title
+    # Get the elements of the article that you need
+    id = article.get('id')
+    article_title = article('a')[0].getText().encode('utf-8')
+    date = article('time')[0].get('datetime').encode('utf-8')
+    link = 'http:'+article('a')[0].get('href').encode('utf-8')
+    mdown_link = '1. [%s](%s)' % (article_title,link)
+    year = int(date.split('-')[0])
+    month = date.split('-')[1]
+    if year = 2016
+      ts_posts.append(mdown_link.strip("'"))
+    elif year = 2015
+      tf_posts.append(mdown_link.strip("'"))
+    else 
+      continue "%s didn't have a recent year" % (article_title)
 
-protocol = 'http:'
-mdown_link = '1. [%s](%s)' % (article_title,full_url)
-title_list.append(mdown_link.strip("'"))
 
+
+    if month == '1':
+      month_name == 'January'
+    elif month == '2':
+      month_name == 'February'
+    elif month == '3':
+      month_name == 'March'
+    elif month == '4':
+      month_name == 'April'
+    elif month == '5':
+      month_name == 'May'
+    elif month == '6':
+      month_name == 'June'
+    elif month == '7':
+      month_name == 'July'
+    elif month == '8':
+      month_name == 'August'
+    elif month == '9':
+      month_name == 'September'
+    elif month == '10':
+      month_name == 'October'
+    elif month == '11':
+       month_name == 'November'
+    elif month == '12':
+      month_name == 'December'
+
+    ad = {
+      'title': article_title,
+      'month': month_name
+    }
+    print ad
+
+  except Exception as e:
+      pass
+
+
+
+print mdown_link
+
+    
 
 """
 *--final formatting--*
