e07416bba29ec124410d31e5dbf78b45f688aa8a
wcpines <colby@aptible.com>
2016-05-31 11:20:58 -0400

    Getting closer...

--- inconscrapuous.py
+++ inconscrapuous.py
@@ -2,6 +2,7 @@
 import requests
 import bs4
 import time
+import datetime
 
 
 """
@@ -33,76 +34,27 @@ for article in articles:
   try:
 
     # Get the elements of the article that you need
-    id = article.get('id')
-    article_title = article('a')[0].getText().encode('utf-8')
-    date = article('time')[0].get('datetime').encode('utf-8')
-    link = 'http:'+article('a')[0].get('href').encode('utf-8')
-    mdown_link = '1. [%s](%s)' % (article_title,link)
-    year = int(date.split('-')[0])
-    month = date.split('-')[1]
-    if year = 2016
-      ts_posts.append(mdown_link.strip("'"))
-    elif year = 2015
-      tf_posts.append(mdown_link.strip("'"))
-    else 
-      continue "%s didn't have a recent year" % (article_title)
+    id = article.get('id') # get the article ID
+    article_title = article('a')[0].getText().encode('utf-8') # get the article title
+    datestring = article('time')[0].get('datetime').encode('utf-8') # get the datetime--it returns a sting
+    datetime = datetime.datetime.strptime(datestring, "%Y-%m-%d") # get the datestring to an actual datetime data type
+    month = new.strftime("%B") # get the month name from it
+    link = 'http:'+article('a')[0].get('href').encode('utf-8') # get the anchor link, append protocol 
+    mdown_link = '1. [%s](%s)' % (article_title,link) # create markdown hyperlink using href and title text
+    year = int(datestring.split('-')[0])
+    if year == 2016:
+      ts_posts.append(mdown_link.strip("'") + ';' + month)
+    elif year == 2015:
+      tf_posts.append(mdown_link.strip("'") + ';' + month)
+    else:
+      print "%s didn't have a recent year" % (article_title)
 
-
-
-    if month == '1':
-      month_name == 'January'
-    elif month == '2':
-      month_name == 'February'
-    elif month == '3':
-      month_name == 'March'
-    elif month == '4':
-      month_name == 'April'
-    elif month == '5':
-      month_name == 'May'
-    elif month == '6':
-      month_name == 'June'
-    elif month == '7':
-      month_name == 'July'
-    elif month == '8':
-      month_name == 'August'
-    elif month == '9':
-      month_name == 'September'
-    elif month == '10':
-      month_name == 'October'
-    elif month == '11':
-       month_name == 'November'
-    elif month == '12':
-      month_name == 'December'
-
-    ad = {
-      'title': article_title,
-      'month': month_name
-    }
-    print ad
+    for post in tf_posts:
+      print post
+    
+    for post in ts_posts:
+      print post
 
   except Exception as e:
       pass
-
-
-
-print mdown_link
-
-    
-
-"""
-*--final formatting--*
-#Archive 
-
-##Posts in [year]
-
-###Month 1
-
--Article link 
--Article link 
-
-###Month 2
-
--Article link 
--Article link 
-"""
 
